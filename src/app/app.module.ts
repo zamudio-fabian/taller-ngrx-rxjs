70cb797ee6b2f2b3ca908ce2de46bb16f0bb8222
fzamudio <fabian@qrsolutions.com.ar>
2021-09-15 09:11:21 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import * as CharactersReducer from './reducers/character.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+import { RickAndMortyEffects } from './effects/rickandmorty.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ characters: CharactersReducer.reducer}, {}),
+    EffectsModule.forRoot([RickAndMortyEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
